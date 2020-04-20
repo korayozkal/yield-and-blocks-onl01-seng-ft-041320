f8d3a3fca7502d8c99cddfe9fd245ef812ebcdae
Koray Ozkal <koryozkal@gmail.com>
2020-04-20 22:58:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,17 @@
-def hello_t
+
+#   ["Tim", "Tom", "Jim"].each do |name|
+#   name.start_with?("T")
+#   puts "Hi, #{name}"
+#   end
+#   end
+
+
+def hello_t(array)
+i = 0
+while i < array.length
+i = i + 1
+end
+end
 
 end
 
